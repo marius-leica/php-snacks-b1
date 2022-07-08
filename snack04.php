5ca45e4c66abc7b150c4d0f1f7ea1fbf50ec73fa
Marius L <mariusleica@gmail.com>
2022-07-08 16:25:19 +0200

    snack 04

--- snack04.php
+++ snack04.php
@@ -2,3 +2,39 @@
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
 
 <?php
+$randomNumbers = [];
+while (count($randomNumbers) < 15) {
+    $randomNumber = rand(0, 100);
+    if (!in_array($randomNumber, $randomNumbers)) {
+        $randomNumbers[] = $randomNumber;
+    }
+}
+// var_dump($randomNumbers);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="numbers.ico" rel="icon" type="image/x-icon" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous' />
+    <title>Random numbers</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-4">
+            Your random numbers:
+        </h1>
+        <div class="text-center">
+            <?php echo implode(', ', $randomNumbers); ?>
+
+
+        </div>
+
+</body>
+
+</html>
--- snack07.php
+++ snack07.php
@@ -2,3 +2,29 @@
 Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
 
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="favicon.ico" rel="icon" type="image/x-icon" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous' />
+    <title>NBA Finals Golden State Warriors VS Boston Celtics</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-4">
+            NBA Finals Golden State Warriors VS Boston Celtics
+        </h1>
+
+    </div>
+
+</body>
+
+</html>
