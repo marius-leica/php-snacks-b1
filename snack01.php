51a74f0aeaae6d69b17655ba0d81b2afd5b04247
Marius L <mariusleica@gmail.com>
2022-07-08 15:34:50 +0200

    first snack

--- snack01.php
+++ snack01.php
@@ -4,3 +4,93 @@ Olimpia Milano - Cantù | 55-60 -->
 
 
 <?php
+
+$nbaCalendar = [
+    [
+        'home' => 'Golden State Warriors',
+        'guest' => 'Boston Celtics',
+        'homePoints' => 108,
+        'guestPoints' => 120
+    ],
+    [
+        'home' => 'Golden State Warriors',
+        'guest' => 'Boston Celtics',
+        'homePoints' => 107,
+        'guestPoints' => 88
+    ],
+    [
+        'home' => 'Boston Celtics',
+        'guest' => 'Golden State Warriors',
+        'homePoints' => 116,
+        'guestPoints' => 100
+    ],
+    [
+        'home' => 'Boston Celtics',
+        'guest' => 'Golden State Warriors',
+        'homePoints' => 97,
+        'guestPoints' => 107
+    ],
+    [
+        'home' => 'Golden State Warriors',
+        'guest' => 'Boston Celtics',
+        'homePoints' => 104,
+        'guestPoints' => 94
+    ],
+    [
+        'home' => 'Boston Celtics',
+        'guest' => 'Golden State Warriors',
+        'homePoints' => 90,
+        'guestPoints' => 103
+    ],
+];
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="favicon.ico" rel="icon" type="image/x-icon" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous' />
+    <title>NBA Finals Golden State Warriors VS Boston Celtics</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-4">
+            NBA Finals Golden State Warriors VS Boston Celtics
+        </h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Home</th>
+                    <th>Guest</th>
+                    <th>Home Points</th>
+                    <th>Guest Points</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($nbaCalendar as $game) : ?>
+                    <tr>
+                        <td><?= $game['home'] ?></td>
+                        <td><?= $game['guest'] ?></td>
+                        <td><?= $game['homePoints'] ?></td>
+                        <td><?= $game['guestPoints'] ?></td>
+
+                    </tr>
+
+                <?php endforeach; ?>
+
+            </tbody>
+    </div>
+
+</body>
+
+</html>
